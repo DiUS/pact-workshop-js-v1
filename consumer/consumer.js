a678e384b194809e2812b66b79abac083486e242
Matt Fellows <matt.fellows@onegeek.com.au>
2017-06-12 16:31:41 +1000

    feat(step1): add simple provider and client

---
+++ consumer/consumer.js
@@ -0,0 +1,12 @@
+const request = require('superagent')
+const API_HOST = process.env.API_HOST || 'http://localhost'
+const API_PORT = process.env.API_PORT || 8080
+const API_ENDPOINT = `${API_HOST}:${API_PORT}`
+
+// Fetch provider data
+request
+  .get(`${API_ENDPOINT}/provider`)
+  .query({validDate: new Date().toISOString()})
+  .then((res) => {
+    console.log(res.body)
+  })
---
+++ provider/provider.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const server = express()
+const port = 8080 || process.env.API_PORT
+
+server.use(cors())
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use((req, res, next) => {
+  res.header('Content-Type', 'application/json; charset=utf-8')
+  next()
+})
+
+// Get all animals
+server.get('/provider', (req, res) => {
+  const date = req.query.validDate
+
+  res.json(
+    {
+      'test': 'NO',
+      'validDate': new Date().toISOString(),
+      'count': 100
+    }
+  )
+})
+
+server.listen(port, () => {
+  console.log(`Provider Service listening on http://localhost:${port}`)
+})
