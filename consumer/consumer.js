8b7c566dbc3aa95ab2075b0ebd98a5b9d1ec33b5
Matt Fellows <matt.fellows@onegeek.com.au>
2017-08-06 07:56:41 +1000

    chore(port): update port to 9123 to avoid conflicts

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -1,6 +1,6 @@
 const request = require('superagent')
 const API_HOST = process.env.API_HOST || 'http://localhost'
-const API_PORT = process.env.API_PORT || 8080
+const API_PORT = process.env.API_PORT || 9123
 const API_ENDPOINT = `${API_HOST}:${API_PORT}`
 
 // Fetch provider data
