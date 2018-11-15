7c51ad85754bb8c69e5a90eecfbb5584ffa1d08c
Matt Fellows <matt.fellows@onegeek.com.au>
2018-11-14 22:18:27 -1000

    fix(date): fix date format for negative TZs

--- consumer/client.js
+++ consumer/client.js
@@ -13,7 +13,7 @@ const fetchProviderData = submissionDate => {
       // Validate date
       if (
         res.body.validDate.match(
-          /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}/
+          /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\+|\-)\d{2}:\d{2}/
         )
       ) {
         return {
